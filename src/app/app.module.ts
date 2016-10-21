ae91ea6924fbe3e67519f80c8b33bdcb0b443592
nydal91 <nydal91@hotmail.com>
2016-10-21 21:13:44 +0200

    modified:   angular-cli.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/footer/footer.component.css 	new file:   src/app/footer/footer.component.html 	new file:   src/app/footer/footer.component.spec.ts 	new file:   src/app/footer/footer.component.ts 	new file:   src/app/jumbotron/jumbotron.component.css 	new file:   src/app/jumbotron/jumbotron.component.html 	new file:   src/app/jumbotron/jumbotron.component.spec.ts 	new file:   src/app/jumbotron/jumbotron.component.ts 	new file:   src/app/navbar/navbar.component.css 	new file:   src/app/navbar/navbar.component.html 	new file:   src/app/navbar/navbar.component.spec.ts 	new file:   src/app/navbar/navbar.component.ts 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { CollapseModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+      AlertModule,
+    CollapseModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
